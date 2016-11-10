353c7e2c5990de8a9c7758bce415418fa64fe0a7
Jesus Roman <jesusroman2013@gmail.com>
2016-11-10 17:21:25 -0600

    Add coffee machine class, initialized with inventory

---
+++ models/coffeemachine.rb
@@ -0,0 +1,9 @@
+class BaristaMatic
+
+
+  def initialize
+    @inventory = { "Cocoa" => 10, "Coffee" => 10, "Cream" => 10, "Decaf Coffee" => 10, "Espresso" => 10, "Foamed Milk" => 10, "Steamed Milk" => 10, "Sugar" => 10, "Whipped Cream" => 10 }
+  end
+
+
+end
