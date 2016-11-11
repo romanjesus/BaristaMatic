cfa8ab1eb5a924696c2e1adc928f6b0100d0acab
Jesus Roman <jesusroman2013@gmail.com>
2016-11-10 21:34:55 -0600

    Add menu to initialization, pass menu test

--- models/barista_matic.rb
+++ models/barista_matic.rb
@@ -1,5 +1,5 @@
 class BaristaMatic
-  attr_accessor :inventory
+  attr_accessor :inventory, :menu
   def initialize
     @inventory = { "Cocoa" => 10, "Coffee" => 10, "Cream" => 10, "Decaf Coffee" => 10, "Espresso" => 10, "Foamed Milk" => 10, "Steamed Milk" => 10, "Sugar" => 10, "Whipped Cream" => 10 }
     @menu = [{"Caffe Americano" => "$3.30"}, {"Caffe Latte" => "$2.55"}, {"Caffe Mocha" => "$3.35"}, {"Cappucino" => "$2.90"}, {"Coffee" => "$2.75"}, {"Decaf Coffee" => "$2.75"}]
